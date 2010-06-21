e054b6e1345002110532e66b747772f374168d97
Yann Lugrin <yann.lugrin@sans-savoir.net>
2010-06-21 11:46:16 +0200

    testing loading of library

--- jspec/unit/spec.js
+++ jspec/unit/spec.js
@@ -1,4 +1,27 @@
 
 describe 'jspec-jquery-sandbox'
+
+  describe 'not loaded'
+
+    it 'should throw error if jQuery is not loaded'
+      -{ eval(JSpec.load('../lib/jspec.jquery.sandbox.js')) }.should.throw_error /when using jspec.jquery.sandbox.js/
+    end
+
+    it 'should be loaded if jQuery is loaded'
+      eval(JSpec.load('./support/jquery.js'))
+      -{ eval(JSpec.load('../lib/jspec.jquery.sandbox.js')) }.should_not.throw_error
+    end
+
+  end
+
+  describe 'loaded'
+
+    before_each
+      eval(JSpec.load('./support/jquery.js'))
+      eval(JSpec.load('./lib/jspec.jquery.js'))
+      eval(JSpec.load('../lib/jspec.jquery.sandbox.js'))
+    end
+
+  end
 end
 
